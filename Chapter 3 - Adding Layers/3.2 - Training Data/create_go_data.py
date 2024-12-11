10b795ae9b15daeaa7b6d7a5874074190d77e383
Autocrit <Autocrit@users.noreply.github.com>
2024-12-11 16:58:49 +0000

    Spiral data as mat.Dense

--- Chapter 3 - Adding Layers/3.2 - Training Data/create_go_data.py
+++ Chapter 3 - Adding Layers/3.2 - Training Data/create_go_data.py
@@ -1,25 +1,21 @@
 from spiral_data import spiral_data
 
-# /usr/bin/python3 create_go_data.py > data.go
+# /usr/bin/python3 create_go_data.py > spiral_data.go
 
 points = 100
 classes = 3
 
 X, y = spiral_data(points, classes)
 
-# Write as Go array
-print("rows := " + str(points * classes))
-print("cols := 2")
-print("data := []float64{")
+print("package main\n")
+print("import \"gonum.org/v1/gonum/mat\"\n")
+print("func SpiralData() (*mat.Dense, *mat.Dense) {")
+print("\tX := mat.NewDense(" + str(points*classes) + ", 2, []float64{")
 for v in X:
-	print("\t" + str(v[0]) + ", " + str(v[1]) + ",")
-print("}")
+	print("\t\t" + str(v[0]) + ", " + str(v[1]) + ",")
+print("\t})\n")
 
-print("")
-
-#print("var rows = " + str(points * classes))
-#print("var cols = 1")
-#print("var data = []float64{")
-#for v in y:
-#    print(str(v) + ", ", end="")
-#print("}")
+print("\ty := mat.NewDense(" + str(points*classes) + ", 1, []float64{", end="")
+for v in y:
+    print(str(v) + ", ", end="")
+print("})\n\n\treturn X, y\n}")
