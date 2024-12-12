f1e77a2336a18097ce51633d23832b7db227b316
Autocrit <Autocrit@users.noreply.github.com>
2024-12-12 12:40:36 +0000

    Initiate matrices

--- Misc/Array vs Gonum/matrix_test.go
+++ Misc/Array vs Gonum/matrix_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
 
 	"gonum.org/v1/gonum/mat"
@@ -16,21 +17,33 @@ var ca = 16 // Matrix a cols
 var rb = ca // Matrix b rows
 var cb = 12 // Matrix b cols
 
+func rand_array(n int) []float64 {
+	result := make([]float64, n)
+	for i, _ := range result {
+		result[i] = rand.Float64()
+	}
+	return result
+}
+
 func BenchmarkDot1D(b *testing.B) {
 	ma := NewMatrix(ra, ca)
+	ma.data = rand_array(ra * ca)
+
 	mb := NewMatrix(rb, cb)
-	mc := NewMatrix(ra, cb)
+	mb.data = rand_array(rb * cb)
+
+	m := NewMatrix(ra, cb)
 
 	for i := 0; i < b.N; i++ {
-		mc.Dot(ma, mb)
+		m.Dot(ma, mb)
 	}
 
-	result = mc.At(0, 0)
+	result = m.At(0, 0)
 }
 
 func BenchmarkMatDense(b *testing.B) {
-	ma := mat.NewDense(ra, ca, nil)
-	mb := mat.NewDense(rb, cb, nil)
+	ma := mat.NewDense(ra, ca, rand_array(ra*ca))
+	mb := mat.NewDense(rb, cb, rand_array(rb*cb))
 	mc := mat.NewDense(ra, cb, nil)
 
 	for i := 0; i < b.N; i++ {
