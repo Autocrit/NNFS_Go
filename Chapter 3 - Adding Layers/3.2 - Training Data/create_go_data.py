11860110ba665eb781a25754f892c8b7d6299fa9
Autocrit <Autocrit@users.noreply.github.com>
2024-12-12 12:39:07 +0000

    rename spiral_data function

--- Chapter 3 - Adding Layers/3.2 - Training Data/create_go_data.py
+++ Chapter 3 - Adding Layers/3.2 - Training Data/create_go_data.py
@@ -9,7 +9,7 @@ X, y = spiral_data(points, classes)
 
 print("package main\n")
 print("import \"gonum.org/v1/gonum/mat\"\n")
-print("func SpiralData() (*mat.Dense, *mat.Dense) {")
+print("func spiral_data() (*mat.Dense, *mat.Dense) {")
 print("\tX := mat.NewDense(" + str(points*classes) + ", 2, []float64{")
 for v in X:
 	print("\t\t" + str(v[0]) + ", " + str(v[1]) + ",")
