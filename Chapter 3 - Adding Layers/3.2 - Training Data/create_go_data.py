577276e484e16069dedb26b6097a458836ab184c
Autocrit <Autocrit@users.noreply.github.com>
2024-12-13 15:05:16 +0000

    y from spiral_data.go should be array of ints

--- Chapter 3 - Adding Layers/3.2 - Training Data/create_go_data.py
+++ Chapter 3 - Adding Layers/3.2 - Training Data/create_go_data.py
@@ -9,13 +9,13 @@ X, y = spiral_data(points, classes)
 
 print("package main\n")
 print("import \"gonum.org/v1/gonum/mat\"\n")
-print("func spiral_data() (*mat.Dense, *mat.Dense) {")
+print("func spiral_data() (*mat.Dense, []int) {")
 print("\tX := mat.NewDense(" + str(points*classes) + ", 2, []float64{")
 for v in X:
 	print("\t\t" + str(v[0]) + ", " + str(v[1]) + ",")
 print("\t})\n")
 
-print("\ty := mat.NewDense(" + str(points*classes) + ", 1, []float64{", end="")
+print("\ty := []int{", end="")
 for v in y:
     print(str(v) + ", ", end="")
-print("})\n\n\treturn X, y\n}")
+print("}\n\n\treturn X, y\n}")
